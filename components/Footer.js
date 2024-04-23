ac09706d9329ab8dfd7181f6cf288e9d54f90bcc
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-04-23 23:02:43 +0530

    Components-Nav-Footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-white px-4 py-2 text-center absolute bottom-0 w-full'>
+      <p>Copyright &copy; Get me a Coffee- fund your projects with coffee - All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
